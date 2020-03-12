e3f0485497b437f80683da501429e47e40e2c828
Blackghost58 <62009328+Blackghost58@users.noreply.github.com>
2020-03-12 07:35:24 +0500

    Add files via upload

---
+++ abonent/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Abonent(models.Model):
+    Name=models.CharField(max_length=30)
+    Surname=models.CharField(max_length=30)
+    Secondname=models.CharField(max_length=30)
+    Rank=models.CharField(max_length=30)
+    Tel=models.CharField(max_length=30)
+    Ip_tel=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.Name
+# Create your models here.
